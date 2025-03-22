2f97a31c74d008c29abf557298ebea0087a8d677
shinny <shinnyoyere@gmail.com>
2025-03-22 21:07:32 +0100

    Création de la structure de la Treemap SCRUM-48

--- Data Visualization with D3/Treemap/treemap.js
+++ Data Visualization with D3/Treemap/treemap.js
@@ -7,3 +7,38 @@ d3.json(movieDataUrl).then(data => {
   const svg = d3.select("#treemap");
   const width = svg.attr("width");
   const height = svg.attr("height");
+
+  // Set up the Treemap layout
+  const treemap = d3.treemap()
+    .size([width, height])
+    .padding(1);
+
+  // Set up the color scale for genres
+  const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+
+  // Process the data into a hierarchy
+  const root = d3.hierarchy(data)
+    .sum(d => d.value); // Sum the values for each node
+
+  // Create the Treemap nodes
+  treemap(root);
+
+  // Create the rectangles (tiles)
+  svg.selectAll(".tile")
+    .data(root.leaves())
+    .enter().append("rect")
+    .attr("class", "tile")
+    .attr("x", d => d.x0)
+    .attr("y", d => d.y0)
+    .attr("width", d => d.x1 - d.x0)
+    .attr("height", d => d.y1 - d.y0)
+    .style("fill", d => colorScale(d.data.category))
+    .attr("data-name", d => d.data.name)
+    .attr("data-category", d => d.data.category)
+    .attr("data-value", d => d.data.value)
+    .on("mouseover", function(event, d) {
+      const tooltip = d3.select("#tooltip");
+      tooltip.style("visibility", "visible")
+        .attr("data-value", d.data.value)
+        .html(`${d.data.name}<br>${d.data.category}<br>$${d.data.value.toLocaleString()}`);
+    })
